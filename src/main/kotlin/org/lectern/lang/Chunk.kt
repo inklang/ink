e3e735744b55acbcf926c0aa3e0ee163677f19c0
mintychochip <jlo2@csub.edu>
2026-03-17 01:32:31 -0700

    feat: add spillSlotCount field to Chunk

--- src/main/kotlin/org/lectern/lang/Chunk.kt
+++ src/main/kotlin/org/lectern/lang/Chunk.kt
@@ -26,6 +26,7 @@ class Chunk {
     val functions = mutableListOf<Chunk>()
     val classes = mutableListOf<ClassInfo>()
     val functionDefaults = mutableListOf<FunctionDefaults>()  // Parallel to functions - defaults for each function
+    var spillSlotCount: Int = 0
 
     fun addConstant(value: Value): Int {
         constants.add(value)
